1ef792a4e7c2268657a888e597b99934fe41a7e2
catarestrepo1401 <53926855+catarestrepo1401@users.noreply.github.com>
2019-11-29 15:36:18 -0500

    second commit

---
+++ database/migrations/2019_11_29_200142_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('Names');
+            $table->string('Last Name');
+            $table->string('Adress');
+            $table->string('Cellphone');
+            $table->string('Email');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
